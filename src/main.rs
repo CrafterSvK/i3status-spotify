ed77f3a20dc54d058f4008b76c4bb3d08552a6dd
Jakub Janek <shaman.janek@gmail.com>
2019-07-24 20:11:22 +0200

    Refactor, removed regex dependency

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-extern crate regex;
-
 use std::{str, io};
 use std::process::Command;
-use regex::Regex;
 
 fn main() -> io::Result<()> {
 	let mut buffer = String::new();
-  let stdin = io::stdin();
+	let stdin = io::stdin();
 
-	//could be optimized
-	let artist = Regex::new(r"xesam:artist[ ]+([\pLN ]+)").unwrap();
-	let title = Regex::new(r"xesam:title[ ]+([\pLN ]+)").unwrap();
-	
-	//remove version header and start of an array
+	//first 2 are headers and third contains [ as a start of array
 	for _ in 0..3 {
 		buffer.clear();
 	 	stdin.read_line(&mut buffer)?;
@@ -23,27 +16,42 @@ fn main() -> io::Result<()> {
 		buffer.clear();
  		stdin.read_line(&mut buffer)?;
 
-		//playerctl can't detect if is spotify playing or it has it paused. DBUS required.
-		let meta = Command::new("/bin/playerctl")
-			.arg("metadata")
-			.output()
-			.expect("Playerctl failed or is not installed")
-			.stdout;
+		if playerctl!("status").as_str() == "Playing" {
+			let title = playerctl!("metadata", "title"); 
+			let artist = playerctl!("metadata", "artist");
+		
+			//unsafe but works, because those are ASCII chars.
+			let json_parsable = &buffer.as_str()[2..];
 
-		let metadata = match str::from_utf8(meta.as_slice()) {
-			Ok(metadata) => metadata,
-			Err(_) => panic!("Playerctl failed to give metadata")
-		};
+			let spotify = format!("{} - {}", artist.as_str(), title.as_str());
 
-		//unsafe but works, because those are ASCII chars.
-		let json_parsable = &buffer.as_str()[2..];
-
-		let current_artist = &artist.captures(metadata).unwrap()[1];
-		let current_title = &title.captures(metadata).unwrap()[1];
-
-		let spotify = format!("{} - {}", current_artist, current_title);
-
-		//appends only on first position :(, and it's tricky, + doesn't require json library	
-		print!(",[{{\"name\":\"spotify\",\"color\":\"#00FF00\",\"full_text\":\"{}\"}},{}", spotify, json_parsable)
+			//appends only on first position :(, and it's tricky, + doesn't require json library	
+			print!(",[{{\"name\":\"spotify\",\"color\":\"#00FF00\",\"full_text\":\"{}\"}},{}", spotify, json_parsable);
+		} else {	
+			print!("{}", buffer);
+		}
 	}
+}
+
+#[macro_export]
+macro_rules! playerctl {
+	( $( $x:expr ),*) => {
+		{
+			let mut command = Command::new("/bin/playerctl");
+
+			$(
+				command.arg($x);
+			)*
+
+			let data = command
+				.output()
+				.expect("Playerctl is not installed or doesn't work correctly")
+				.stdout;
+
+			match str::from_utf8(data.as_slice()) {
+				Ok(meta) => meta.trim().to_string(),
+				Err(_) => panic!("Playerctl failed to give metadata")
+			}			
+		}
+	};
 }
